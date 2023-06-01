c005943f1d38c3e948dedc8f055fb2b021f730e3
Romnick Agsaoay <romnick.agsaoay@googlemail.com>
2023-06-01 19:56:57 +0100

    Added Users,Leave types, and  Leave allocations tables.

---
+++ LeaveManagement.Web/Data/Migrations/20230601182532_EditUserTable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagement.Web.Data.Migrations
+{
+    public partial class EditUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TaxId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TaxId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
